51dcebab44c6a56cf1f105d414b20f0472273cbd
nieSuv <huusangvtvip@gmail.com>
2023-01-04 21:54:17 +0700

    small bug

--- cheems.py
+++ cheems.py
@@ -123,7 +123,8 @@ def sub_game(width, height, gold):
 	while running:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				running = False
+				pygame.quit()
+				sys.exit()
 		
 		# Control the Dog
 		keys = pygame.key.get_pressed()
